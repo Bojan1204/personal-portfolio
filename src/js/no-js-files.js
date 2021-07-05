69dd96c67f894104389f96f3fd5e9dd6319abc8a
Bojan Bosnic <56975621+Bojan1204@users.noreply.github.com>
2021-07-05 14:11:24 +0200

    addded cv

--- src/js/no-js-files.js
+++ src/js/no-js-files.js
@@ -4,7 +4,7 @@
 import '../scss/index.scss';
 import '../img/icons/favicon.ico';
 import '../img/icons/favicon.png';
-import '../pdf/Bojan-Bosnic-CV.pdf';
+import '../pdf/Bojan-Bosnic_CV.pdf';
 import '../../.gitkeep';
 import '../.htaccess';
 import '@fortawesome/fontawesome-free/js/all.js';
